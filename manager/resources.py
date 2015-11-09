31533e236e88ce22512b9fd68459e7c9bab4b10d
joaopaulo164 <joaopaulo164@hotmail.com>
2015-11-09 13:26:23 -0200

    API com nova modelagem do banco

--- manager/resources.py
+++ manager/resources.py
@@ -1,10 +1,66 @@
 __author__ = 'joao'
 
-from tastypie.resources import ModelResource
-from manager.models import Cliente
+from django.contrib.auth.models import User
+from tastypie.authorization import Authorization
+from tastypie import fields
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from manager.models import Cliente, Funcionario, Historico, Servico
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        filtering = {
+            'username': ALL,
+        }
 
 
 class ClienteResource(ModelResource):
+
     class Meta:
         queryset = Cliente.objects.all()
-        allowed_methods = ['get']
+        resource_name = 'cliente'
+        authorization = Authorization()
+        filtering = {
+            'nome': ALL,
+            #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }
+
+class FuncionarioResource(ModelResource):
+
+    class Meta:
+        queryset = Funcionario.objects.all()
+        resource_name = 'funcionario'
+        authorization = Authorization()
+        filtering = {
+            'nome': ALL,
+            #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }
+
+class ServicoResource(ModelResource):
+
+    class Meta:
+        queryset = Servico.objects.all()
+        resource_name = 'servico'
+        authorization = Authorization()
+        filtering = {
+            'nome': ALL,
+            #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }
+
+class HistoricoResource(ModelResource):
+    cliente = fields.ForeignKey(ClienteResource, 'cliente', full=True)
+    funcionario = fields.ForeignKey(FuncionarioResource, 'funcionario', full=True)
+    servicos = fields.ToManyField(ServicoResource, 'servico', full=True, null=True)
+    # profile = fields.ToOneField('resources.UserProfileResource', attribute = 'userprofile', related_name='user', full=True, null=True)
+
+    class Meta:
+        queryset = Historico.objects.all()
+        resource_name = 'historico'
+        authorization = Authorization()
+        filtering = {
+            'cliente': ALL_WITH_RELATIONS,
+            #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }

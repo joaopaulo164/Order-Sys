43fb0ece5477b224457533d026798424225def21
joaopaulo164 <joaopaulo164@hotmail.com>
2015-11-09 16:12:05 -0200

    API com toManyFild OK

--- manager/resources.py
+++ manager/resources.py
@@ -11,6 +11,7 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+        allowed_methods = ['get', 'post', 'delete', 'put']
         excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
         filtering = {
             'username': ALL,
@@ -22,6 +23,7 @@ class ClienteResource(ModelResource):
     class Meta:
         queryset = Cliente.objects.all()
         resource_name = 'cliente'
+        allowed_methods = ['get', 'post', 'delete', 'put']
         authorization = Authorization()
         filtering = {
             'nome': ALL,
@@ -33,34 +35,40 @@ class FuncionarioResource(ModelResource):
     class Meta:
         queryset = Funcionario.objects.all()
         resource_name = 'funcionario'
+        allowed_methods = ['get', 'post', 'delete', 'put']
         authorization = Authorization()
         filtering = {
             'nome': ALL,
             #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
         }
+
 
 class ServicoResource(ModelResource):
 
     class Meta:
         queryset = Servico.objects.all()
         resource_name = 'servico'
+        allowed_methods = ['get', 'post', 'delete', 'put']
         authorization = Authorization()
         filtering = {
             'nome': ALL,
             #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
         }
 
+
 class HistoricoResource(ModelResource):
     cliente = fields.ForeignKey(ClienteResource, 'cliente', full=True)
     funcionario = fields.ForeignKey(FuncionarioResource, 'funcionario', full=True)
-    servicos = fields.ToManyField(ServicoResource, 'servico', full=True, null=True)
-    # profile = fields.ToOneField('resources.UserProfileResource', attribute = 'userprofile', related_name='user', full=True, null=True)
+    servicos = fields.ToManyField(ServicoResource, attribute=lambda bundle: Servico.objects, full=True)
 
     class Meta:
         queryset = Historico.objects.all()
         resource_name = 'historico'
+        allowed_methods = ['get', 'post', 'delete', 'put']
         authorization = Authorization()
         filtering = {
             'cliente': ALL_WITH_RELATIONS,
+            'funcionario': ALL_WITH_RELATIONS,
+            'servicos': ALL_WITH_RELATIONS,
             #'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
         }
